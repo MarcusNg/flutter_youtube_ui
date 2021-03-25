5d41f6011ff8f0645e46b6219dd0e51bac8693cb
Marcus Ng <MarcusNg@users.noreply.github.com>
2021-03-25 12:36:41 -0400

    add starter project

---
+++ lib/data.dart
@@ -0,0 +1,115 @@
+class User {
+  final String username;
+  final String profileImageUrl;
+  final String subscribers;
+
+  const User({
+    required this.username,
+    required this.profileImageUrl,
+    required this.subscribers,
+  });
+}
+
+const User currentUser = User(
+  username: 'Marcus Ng',
+  profileImageUrl:
+      'https://yt3.ggpht.com/ytc/AAUvwniE2k5PgFu9yr4sBVEs9jdpdILdMc7ruiPw59DpS0k=s88-c-k-c0x00ffffff-no-rj',
+  subscribers: '100K',
+);
+
+class Video {
+  final String id;
+  final User author;
+  final String title;
+  final String thumbnailUrl;
+  final String duration;
+  final DateTime timestamp;
+  final String viewCount;
+  final String likes;
+  final String dislikes;
+
+  const Video({
+    required this.id,
+    required this.author,
+    required this.title,
+    required this.thumbnailUrl,
+    required this.duration,
+    required this.timestamp,
+    required this.viewCount,
+    required this.likes,
+    required this.dislikes,
+  });
+}
+
+final List<Video> videos = [
+  Video(
+    id: 'x606y4QWrxo',
+    author: currentUser,
+    title: 'Flutter Clubhouse Clone UI Tutorial | Apps From Scratch',
+    thumbnailUrl: 'https://i.ytimg.com/vi/x606y4QWrxo/0.jpg',
+    duration: '8:20',
+    timestamp: DateTime(2021, 3, 20),
+    viewCount: '10K',
+    likes: '958',
+    dislikes: '4',
+  ),
+  Video(
+    author: currentUser,
+    id: 'vrPk6LB9bjo',
+    title:
+        'Build Flutter Apps Fast with Riverpod, Firebase, Hooks, and Freezed Architecture',
+    thumbnailUrl: 'https://i.ytimg.com/vi/vrPk6LB9bjo/0.jpg',
+    duration: '22:06',
+    timestamp: DateTime(2021, 2, 26),
+    viewCount: '8K',
+    likes: '485',
+    dislikes: '8',
+  ),
+  Video(
+    id: 'ilX5hnH8XoI',
+    author: currentUser,
+    title: 'Flutter Instagram Stories',
+    thumbnailUrl: 'https://i.ytimg.com/vi/ilX5hnH8XoI/0.jpg',
+    duration: '10:53',
+    timestamp: DateTime(2020, 7, 12),
+    viewCount: '18K',
+    likes: '1k',
+    dislikes: '4',
+  ),
+];
+
+final List<Video> suggestedVideos = [
+  Video(
+    id: 'rJKN_880b-M',
+    author: currentUser,
+    title: 'Flutter Netflix Clone Responsive UI Tutorial | Web and Mobile',
+    thumbnailUrl: 'https://i.ytimg.com/vi/rJKN_880b-M/0.jpg',
+    duration: '1:13:15',
+    timestamp: DateTime(2020, 8, 22),
+    viewCount: '32K',
+    likes: '1.9k',
+    dislikes: '7',
+  ),
+  Video(
+    id: 'HvLb5gdUfDE',
+    author: currentUser,
+    title: 'Flutter Facebook Clone Responsive UI Tutorial | Web and Mobile',
+    thumbnailUrl: 'https://i.ytimg.com/vi/HvLb5gdUfDE/0.jpg',
+    duration: '1:52:12',
+    timestamp: DateTime(2020, 8, 7),
+    viewCount: '190K',
+    likes: '9.3K',
+    dislikes: '45',
+  ),
+  Video(
+    id: 'h-igXZCCrrc',
+    author: currentUser,
+    title: 'Flutter Chat UI Tutorial | Apps From Scratch',
+    thumbnailUrl: 'https://i.ytimg.com/vi/h-igXZCCrrc/0.jpg',
+    duration: '1:03:58',
+    timestamp: DateTime(2019, 10, 17),
+    viewCount: '358K',
+    likes: '20k',
+    dislikes: '85',
+  ),
+];
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    return MaterialApp(
+      title: 'Flutter YouTube UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        bottomNavigationBarTheme:
+            const BottomNavigationBarThemeData(selectedItemColor: Colors.white),
+      ),
+      home: Scaffold(),
+    );
+  }
+}
